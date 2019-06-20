dc887bfae2d37c37bba431cdc18856d8ceb42ef3
anonymity <1176989591@qq.com>
2019-06-21 00:56:55 +0800

    feat: 增加公共的上传组件

---
+++ src/components/StandardUpload/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Icon, Modal, Upload, Button } from 'antd';
+
+export default class StandardUpload extends React.PureComponent {
+  static propTypes = {
+    fileType: PropTypes.oneOf(['image', 'file']), // 上传类型
+    fileList: PropTypes.array, // 初试文件列表
+    limit: PropTypes.number, // 最多限制几个
+    onChange: PropTypes.func,
+  };
+
+  static defaultProps = {
+    fileType: 'image',
+    fileList: [],
+    limit: 1,
+    onChange: () => {},
+  };
+
+  state = {
+    previewVisible: false,
+    previewImage: '',
+  };
+
+  handlePreview = file => {
+    const { fileType } = this.props;
+    if (fileType === 'file') return;
+    this.setState({
+      previewImage: file.url || file.thumbUrl,
+      previewVisible: true,
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      previewImage: '',
+      previewVisible: false,
+    });
+  };
+
+  render() {
+    const { fileType, fileList, limit, onChange, ...reset } = this.props;
+    const list = fileList || [];
+    const { previewVisible, previewImage } = this.state;
+    const uploadButton =
+      fileType === 'image' ? (
+        <div>
+          <Icon type="plus" />
+          <div className="ant-upload-text">上传</div>
+        </div>
+      ) : (
+        <Button>
+          <Icon type="upload" /> 上传
+        </Button>
+      );
+    return (
+      <div style={fileType === 'image' ? { minHeight: 112 } : null}>
+        <Upload
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          listType="picture-card"
+          fileList={list}
+          onChange={onChange}
+          onPreview={this.handlePreview}
+          {...reset}
+        >
+          {list.length >= limit ? null : uploadButton}
+        </Upload>
+        <Modal
+          visible={previewVisible}
+          closable={false}
+          destroyOnClose
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <img alt="example" style={{ width: '100%' }} src={previewImage} />
+        </Modal>
+      </div>
+    );
+  }
+}
